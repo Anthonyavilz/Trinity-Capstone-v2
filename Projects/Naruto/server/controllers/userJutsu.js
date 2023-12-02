48ce90ae4fb5fe2c5ae78e17943ca9c8b6cc78ad
Phillip Avila <Anthonyavilz@gmail.com>
2023-12-01 23:20:17 -0600

    set up server for Naruto project. Added endpoints and need to test them out

---
+++ Projects/Naruto/server/controllers/userJutsu.js
@@ -0,0 +1,58 @@
+let userJutsu = [];
+let id = 1;
+
+
+module.exports = {
+    createJu: (req, res) => {
+        const {sealOne, sealTwo, sealThree, sealFour, sealFive, sealSix, jutsuName} = req.body;
+        // console.log(req.body);
+        userJutsu.push({
+            id: id, 
+            sealOne,
+            sealTwo,
+            sealThree,
+            sealFour,
+            sealFive,
+            sealSix,
+            jutsuName
+        })
+        id++;
+        console.log(userJutsu)
+        res.status(200).send(userJutsu);
+    },
+
+    getJu: (req, res) => {
+        res.status(200).send(userJutsu);
+    },
+
+    modifyJu: (req, res) => {
+        const {sealOne, sealTwo, sealThree, sealFour, sealFive, sealSix, jutsuName} = req.body;
+        const updateJutsuID = req.params.id;
+        const jutsuIndex = userJutsu.findIndex(jutsu => jutsu.id == updateJutsuID);
+        let jutsu = userJutsu[jutsuIndex];
+
+        userJutsu[jutsuIndex] = {
+            id: jutsu.id,
+            sealOne: sealOne || jutsu.sealOne,
+            sealTwo: sealTwo || jutsu.sealTwo,
+            sealThree: sealThree || jutsu.sealThree,
+            sealFour: sealFour || jutsu.sealFour,
+            sealFive: sealFive || jutsu.sealFive,
+            sealSix: sealSix || jutsu.sealSix,
+            jutsuName: jutsuName || jutsu.jutsuName
+
+        }
+        res.status(200).send(userJutsu)
+    },
+
+    deleteJu: (req, res) => {
+        const {id} = req.params;
+        console.log(id);
+        const index = userJutsu.findIndex(val => val.id == id)
+        if (index === -1) {
+            return res.status(400).json({message: 'jutsu not found'})
+        }
+        userJutsu.splice(index, 1);
+        res.status(200).send(userJutsu);
+    }
+}
